07d63a9945686367ee62f2c51a61c2a0ef7df6f5
Jani Rahkola <jani.rahkola@iki.fi>
2013-10-22 15:48:09 +0300

    min lein version is 2.0.0

--- project.clj
+++ project.clj
@@ -9,4 +9,5 @@
                  [compojure "1.1.5"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
                    :plugins [[lein-midje "3.1.1"]]}}
+  :min-lein-version "2.0.0"
   :main scoreboard.core)
