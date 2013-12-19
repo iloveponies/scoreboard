a9edc75053469c3f6132b1657ee7fddcdba74d3f
Jani Rahkola <jani.rahkola@iki.fi>
2013-12-19 13:03:28 +0200

    reactored github

--- src/scoreboard/github.clj
+++ src/scoreboard/github.clj
@@ -4,23 +4,20 @@
             [rate-gate.core :as rate]
             [scoreboard.util :as util]))
 
-(def *user* (System/getenv "GITHUB_USER"))
-(def *passwd* (System/getenv "GITHUB_PASSWD"))
+(def ^{:dynamic true} *user* (System/getenv "GITHUB_USER"))
+(def ^{:dynamic true} *passwd* (System/getenv "GITHUB_PASSWD"))
 
 (def raw-call!
   (rate/rate-limit
    (fn [method url parameters]
-     (let [user *user*
-           passwd *passwd*]
-       (util/try-times 5 (fn [] (method url {:query-params parameters
-                                             :basic-auth [user passwd]})))))
+     (util/try-times 5 #(method url {:query-params parameters
+                                     :basic-auth [*user* *passwd*]})))
    5000 (* 1000 60 60)))
 
-(defn github-api!
-  ([parameters & url-fragments]
-     (let [url (apply str "https://api.github.com/"
-                      (interpose "/" url-fragments))]
-       (raw-call! http/get url parameters))))
+(defn api! [parameters & url-fragments]
+  (let [url (apply str "https://api.github.com/"
+                   (interpose "/" url-fragments))]
+    (raw-call! http/get url parameters)))
 
 (defn pull-requests!
   ([owner repo]
@@ -28,32 +25,29 @@
              (pull-requests! owner repo "closed")))
   ([owner repo state]
      (loop [{:keys [links body]}
-            (github-api! {"state" state} "repos" owner repo "pulls")
+            (api! {"state" state} "repos" owner repo "pulls")
             prs []]
-       (if (contains? links :next)
-         (recur (raw-call! http/get (:href (:next links)) {"state" state})
-                (concat prs (json/read-str body)))
-         (concat prs (json/read-str body))))))
+       (let [prs (concat prs (json/read-str body :key-fn keyword))]
+         (if (contains? links :next)
+           (recur (raw-call! http/get (:href (:next links)) {"state" state})
+                  prs)
+           prs)))))
 
 (defn pull-request-number [pull-request]
-  (get pull-request "number"))
+  (:number pull-request))
 
 (defn pull-request-author [pull-request]
-  (get-in pull-request ["user" "login"]))
+  (get-in pull-request [:user :login]))
 
 (defn update-author-cache! [cache owner repo]
-  (let [pull-requests (pull-requests! owner repo)]
-    (swap! cache (fn [cache]
-                   (reduce (fn [cache pr]
-                             (assoc cache
-                               [owner repo (pull-request-number pr)]
-                               (pull-request-author pr)))
-                           cache
-                           pull-requests)))))
+  (doseq [pull-request (pull-requests! owner repo)
+          :let [n (pull-request-number pull-request)
+                author (pull-request-author pull-request)]]
+    (swap! cache assoc [owner repo n] author)))
 
 (let [cache (atom {})]
   (defn pull-request-author! [owner repo number]
-    (if (contains? @cache [owner repo number])
-      (get @cache [owner repo number])
+    (if-let [author (get @cache [owner repo number])]
+      author
       (do (update-author-cache! cache owner repo)
           (get @cache [owner repo number])))))
