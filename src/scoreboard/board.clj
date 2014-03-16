c6d029bd0ea9b486ef9b298aee1de4a5e0e5c203
Jani Rahkola <jani.rahkola@iki.fi>
2014-03-16 04:47:20 +0200

    by-ancestry! for boards

--- src/scoreboard/board.clj
+++ src/scoreboard/board.clj
@@ -2,7 +2,15 @@
 
 (defprotocol Store
   (insert! [store board])
-  (by-name-and-parent-key! [store name parent-key])
-  (by-ancestry! [store name & ancestors]))
+  (by-name-and-parent-key! [store name parent-key]))
 
 (defrecord Board [name parent-key])
+
+(defn by-ancestry! [store name & ancestors]
+  (if (empty? ancestors)
+    (by-name-and-parent-key! store name nil)
+    (when-let [parent (apply by-ancestry!
+                             store
+                             (first ancestors)
+                             (rest ancestors))]
+      (by-name-and-parent-key! store name (:key parent)))))
