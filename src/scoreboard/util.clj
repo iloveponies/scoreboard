309db9d8d4ee592140cdb4325375f2f3ac528504
Jani Rahkola <jani.rahkola@iki.fi>
2014-05-03 11:51:02 +0300

    More general retrying util function.

--- src/scoreboard/util.clj
+++ src/scoreboard/util.clj
@@ -1,20 +1,12 @@
-(ns scoreboard.util
-  (:require [clj-http.client :as http]))
+(ns scoreboard.util)
 
-(defn retry [retries on-fail thunk]
-  (if-let [result (try
-                    [(thunk)]
-                    (catch Exception e
-                      (when (empty? retries)
-                        (throw e))))]
-    (result 0)
-    (do (on-fail)
-        (Thread/sleep (first retries))
-        (recur (rest retries) on-fail thunk))))
-
-(defn retrying-http [method url parameters retries]
-  (let [methods {:get http/get
-                 :post http/post}]
-    (retry retries
-           #(println "http request to" url "failed")
-           #((get methods method) url parameters))))
+(defn retrying [f between-retries]
+  (fn [& args]
+    (loop [between-retries between-retries]
+      (if-let [r (try [(apply f args)]
+                      (catch Exception e
+                        (when (empty? between-retries)
+                          (throw e))))]
+        (r 0)
+        (do (Thread/sleep (first between-retries))
+            (recur (rest between-retries)))))))
